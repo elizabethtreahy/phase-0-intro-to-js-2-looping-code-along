407fef24d40ba545287cdef9c8590bfa99663d2f
Elizabeth <elizabethtreahy@gmail.com>
2022-02-23 19:32:48 -0500

    Looping Code

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solutions in this file
+
+
+const cards = [];
+function writeCards(names, holiday) {
+    for (let i = 0; i < names.length; i++) {
+        cards.push(`Thank you, ${names[i]}, for the wonderful ${holiday} gift!`);
+
+    }
+    console.log(cards);
+    return cards;
+}
+writeCards(names, holiday);
+
+
+function countDown(countDownNumber) {
+    while (countDownNumber >= 0) {
+        console.log(countDownNumber--);
+    }
+}
+countDown (32);
